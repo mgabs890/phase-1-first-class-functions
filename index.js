d45769caa1f48bc121db8afc9e6bd945a6f6e000
Gabriella Delgado <m.gabriella.delgado@gmail.com>
2021-10-13 21:04:03 -0400

    commit tree 416562a4d3d3d054ada9b40d80c958de03b196f3

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+        console.log("named function?");
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){
+        console.log("anonymous function?")
+    }
+}
